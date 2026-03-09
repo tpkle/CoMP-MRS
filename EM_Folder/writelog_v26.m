98ec425fe58ea1e2fc39460fe77a46045f3694fe
EM <eloisemougel@gmail.com>
2026-03-09 19:15:12 +0100

    add disp_writelog_v26.m

---
+++ EM_Folder/writelog_v26.m
@@ -0,0 +1,12 @@
+function [] = disp_writelog_v26(pathOut, nameFile,textTowrite)
+%% writelog_v26 
+% write textTowrite in a file - file should exist before calling this
+% function
+%% E Mougel CIBM 02.2026
+
+disp(textTowrite)
+fileIDLog = fopen(fullfile(pathOut,nameFile),'a')
+fprintf(fileIDLog,'%s\n',textTowrite)
+fclose(fileIDLog)
+
+end
